e9d4573a17edc05c42eb3cbf1fe7ad7259169c99
Benjamin <bnjackson951@gmail.com>
2023-09-10 18:04:51 +0100

    Add a server that starts and listens on port 3000. Export server for other parts of program

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+    console.log(`CLI Login App listening on port ${port}`);
+})
+
+module.exports = {
+    app
+}

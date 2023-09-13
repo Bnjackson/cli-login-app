ad7e20e4534ec0eec48480a4d351fdd72d29efbb
Benjamin <bnjackson951@gmail.com>
2023-09-13 12:18:27 +0100

    Add try catch blocks to main conditional, add comments

--- index.js
+++ index.js
@@ -15,35 +15,44 @@ This program use a REST API to connect to the database, and allows for the user 
 async function main() {
     const usersChoice = await loginOrRegister();
     if (usersChoice === 'login') {
-        await loginModule.loginToAccount();
+        try {
+            await loginModule.userLogin();
+            console.log('Login attempt successful');
+        } catch (error) {
+            console.error(`Error login attempt failed ${error}`);
+            main();
+        }
     } else if (usersChoice === 'register') {
-        await registerModule.registereNewAccount();
+        try {
+            await registerModule.registerAccount();
+            console.log('Account registered successfully');
+        } catch (error) {
+            console.error(`Error login attempt failed ${error}`);
+        }
     }
     userCommands();
     console.log('Thanks for using this program');
 }
 
-//Function to prompt user to choose between login and register
 async function loginOrRegister() {
     console.log(`Do you have an existing account? `);
     const userInput = await inputModule.getUserChoice();
     return userInput;
 }
 
-//Function to handle user commands
 async function userCommands() {
     let anotherCommand = true;
     console.log(`
     While you are logged in you have access to the following commands.
     get - get account details,
     put - update account details, 
-    delete - delete account details,
-    These commands will 
+    delete - delete account details
     `);
+    //Another command will remain true until user no longer wishes to continue adding commands. Controlled by continueRunning function.
     while (anotherCommand) {
         const userCommand = await inputModule.getUserCommand();
         if (userCommand === 'get') {
-            await userCommandsModule.get();
+            await userCommandsModule.getUserInfo();
         } else if (userCommand === 'put') {
             await userCommandsModule.put();
         } else if (userCommand === 'delete') {
@@ -53,7 +62,6 @@ async function userCommands() {
     };
 }
 
-//Function to ask the user if they want to continue running commands 
 async function continueRunning() {
     const anotherCommand = await inputModule.getUserInput('Do you want to enter another command? y/n ', 'string');
     if (anotherCommand === 'y') {
